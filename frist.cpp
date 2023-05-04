50b6038f06bef3376879a65b6ca879263598bb72
lord2anil <a8208226@gmail.com>
2023-05-04 16:09:09 +0530

    update

--- frist.cpp
+++ frist.cpp
@@ -1,25 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int T;
-    cin >> T;
-    for (int t = 1; t <= T; t++) {
-        int n;
-        cin >> n;
-        int i = 1, len = 0;
-         while (len < n) {
-            len += i * 26;
-            i++;
-        }
-         i--;
-        len -= i * 26;
-        n -= len;
-        int k = n % i;
-        if (k == 0) k = i;
-        int c = (n - 1) / i;
-        char ans = 'A' + c;
-        cout << "Case #" << t << ": " << ans << endl;
-    }
-    return 0;
-}
+#define int long long
+template<typename... T>
+void put1(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+signed main()
+{ 
+int n;
+cout<<"anil";
+  return 0;}
