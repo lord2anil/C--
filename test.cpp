80ae32f6268899792c62e9de84ce20360d4f4bb5
lord2anil <a8208226@gmail.com>
2023-06-02 10:28:17 +0530

    update

--- test.cpp
+++ test.cpp
@@ -1,25 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
+#define sz(x) (int)x.size()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repd(i,a,b) for(int i=a;i>=b;i--)
+#define trav(a,x) for(auto &a:x)
+#define fi first
+#define se second
+#define mod 1000000007
+#define mod2 998244353
+#define inf 1e18
+#define eps 1e-9
 using namespace std;
+#define int long long
+template<typename... T>
+void put1(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+         int n=nums.size();
+         int ans=0;
+         map<int,int>mp;
+         int sum=0;
+         for (int i = 0; i < n; i++)
+         {sum+=nums[i];
+          int x=sum-k;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    long long ans = 0; // using long long to avoid integer overflow
-    sort(arr.begin(),arr.end());
-    for (int i = 0; i < n; i++) {
+          if(mp.find(x)!=mp.end()){
+            ans+=mp[x];
+          }
+          mp[sum]++;
+         
+         }
+         
+         
         
-        for (int j = i; j < n; j++) {
-            
-            ans += (arr[j]-arr[i]);
-        }
+        return ans;
+        
     }
-    cout << ans << endl;
-    return 0;
-}
+};
+signed main()
+{ 
+int n;
+  return 0;}
