4e6c1282659fb8a2334d5ae1378f8969a748ca62
lord2anil <a8208226@gmail.com>
2024-09-17 23:21:36 +0530

    update

--- codes/new.py
+++ codes/new.py
@@ -1,23 +1,36 @@
-import subprocess
-import time
-import sys
-import argparse
 
-# Set up argument parsing
-parser = argparse.ArgumentParser(description='Types a string character by character from a text file. Useful for inputting long strings of text into programs or consoles that don\'t allow copy and paste. Will wait 3 seconds before typing to allow you to alt-tab into the correct window.')
-parser.add_argument('filename', type=str, help='Path to the text file containing the text you wish the auto-typer to input.')
-
-args = parser.parse_args()
-
-# 3 seconds to alt-tab to correct window after running
-time.sleep(3)
-
-# Open the file and read its contents
-with open(args.filename, 'r') as file:
-    text = file.read()
-
-# Type out the text character by character
-for ch in text:
-    subprocess.call(["xdotool", "type", ch])
-    # Increase or decrease the time below to type slower or faster
-    time.sleep(0.001)
+  * if you code become lengthy you approach is wrong
+   * int overflow, array bounds
+    * special cases (n=1?)
+     * do smth instead of nothing and stay organiz
+raints Section (can also be used to solve problems if they are limited)
+// 0. Remember Pre Computation is always helpful
+// 1. If WA comes and you are sure about the solution, then check the constraints & edge cases.
+// 2. If too many people have solved a question you are stuck on, then always remember: The solution is
+//    100% easy and you need to calm down and think EASY, nothing complicated.
+// 3. Read the question again after getting two wrong submissions. You are 100% missing something important
+//    in the question.
+// 4. If TLE comes in question, then check if one of the test cases generate infinite output.
+//    It will give TLE.
+// 5. If question looks unsolvable from front, try going from back. Works mostly from the back.
+// 6. Stuck on implmentation? Think and then implement Data Structures that best fit the q like set,maps,stacks,queue.
+// 7. Agar Question pe stuck ho and primary logic nahi aa raha dimaag mae then test case se ek pattern deduce karne ka try karo
+// 8. Often Parity Is A good factor for obervation or computation
+// 9. Math type questions mein bohot baar logic/pattern binary/2 ki powers se juda hua hota h ,Ex- See binary expansion of i/p and o/p
+//    ; See occurence of some o/p @ some intervals which may be related to bits/powers of 2
+//     or see how much times a particular value is divisible by 2
+// 10.Many Times pattern/answer is related with some special numbers (min,max,both min&max, total sum ,
+//    two i/p x and y of large range then ans related with some operation between x and y && same with 3 no i/p)
+ */
+   // #ifndef ONLINE_JUDGE
+     //freopen("input.txt","r", stdin);
+       //freopen("output.txt","w", stdout);
+         // #ndif
+           int T;
+             int t;
+               cin>>T;
+                 t = T;
+                   while (T--)
+                     { fuck( t, t-Td<<setprecision(10);
+                       // cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<" ms\n";
+                         return 0;})
